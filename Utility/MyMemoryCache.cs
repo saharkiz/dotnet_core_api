a6c16f41945ca0023de9dc66ca1a4cf7e011fecb
Aresh Saharkhiz <saharkiz@yahoo.com>
2020-10-10 20:14:59 +0800

    Add files via upload

---
+++ Utility/MyMemoryCache.cs
@@ -0,0 +1,39 @@
+
+using Microsoft.Extensions.Caching.Memory;
+public interface IMyMemoryCache : IMemoryCache
+{
+    void Reset();
+}
+public class MyMemoryCache: IMyMemoryCache
+{
+    IMemoryCache _memoryCache;
+
+    public MyMemoryCache()
+    {
+        Reset();
+    }
+    public void Dispose()
+    {
+        _memoryCache.Dispose();
+    }
+
+    public bool TryGetValue(object key, out object value)
+    {
+        return _memoryCache.TryGetValue(key, out value);
+    }
+
+    public ICacheEntry CreateEntry(object key)
+    {
+        return _memoryCache.CreateEntry(key);
+    }
+
+    public void Remove(object key)
+    {
+        _memoryCache.Remove(key);
+    }
+
+    public void Reset()
+    {
+        _memoryCache = new MemoryCache(new MemoryCacheOptions());
+    }
+}
